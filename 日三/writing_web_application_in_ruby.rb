a42f00a0bc002607f7b07d56b3acebae96f69a60
Barry <barry.teoh@perceptionz.net>
2015-12-13 14:41:00 +0900

    Day 3

---
+++ 日三/writing_web_application_in_ruby.rb
@@ -0,0 +1,11 @@
+# Notes
+* [Slides](http://rubykaigi.youchan.org/)
+* youchan/Hyalite - Frontend ruby gem which uses react.js
+* Opal gem (https://github.com/opal/opal.git)
+* Can use Hyalite::Component::ShortHand to do it shortform
+* Selecting a [javascript framework](http://todomvc.com/)
+* Based on todoMVC https://github.com/youchan/hyalite-todo
+
+## Future Plans
+* Electron app
+* RubyMotion and HyaliteNative
